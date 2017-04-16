f90b48ea1032eb311ed009dd3f837afd2b1e581d
BAAMOSK <bunth667@gmail.com>
2017-04-16 02:00:38 -0400

    save in progress

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 //STATE
-let state = [
+let state = {
+	quiz: [
 	{ question: 'What is my favorite color?',
 	  choices: ['blue', 'green', 'red', 'purple'],
 	  correctAnswer: 'blue'
@@ -11,17 +12,31 @@ let state = [
 	{ question: 'What is my favorite movie?',
 	  choices: ['Apple Seed','Limitless', 'Split', 'Moana'],
 	  correctAnswer: 'Limitless'
-	}
+	}],
+	score: 0,
+	currentQuestion: 0,
+	correctResponse: `Good Job you got it right` ,
+	wrongResponse: `You got the answer wrong. The correct answer is `
 ];
 
 //STATE MOD FUNCTIONS
 let nextQuestion = function(state, question) {
-	let currentQuestion 
+	
 };
 
-let validateQuestion = function() {};
+let validateQuestion = function(state, question) {
+	if(choices === correctAnswer) {
+		state.score++;
+		showResponse(state.correctResponse);
+	} else {
+		showResponse(state.wrongResponse);
+	}
+};
 
-let restart = function() {};
+let restart = function(state) {
+	state.score = 0;
+	state.currentQuestion = 0;
+};
 
 //RENDER FUNCTIONS
 let showQuestion = function(state, element) {
@@ -32,6 +47,10 @@ let showQuestion = function(state, element) {
 	return element.html(quizHtml);
 };
 
+let showResponse = function(response) {
+	let element = $('.response');
+	return element.html(response);
+}
 
 //EVENT LISTENERS
 $('#start').submit(function() {
